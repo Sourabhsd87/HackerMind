0b0445e13709f58c40fffb75fac1164939c5f423
Sourabh Deshmukh <102124662+Sourabhsd87@users.noreply.github.com>
2023-09-05 19:37:10 +0530

    Solution for Java Substring

---
+++ Java Substring/Solution.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+        for(int i=start;i<end;i++){
+            System.out.print(S.charAt(i));
+        }
+    }
+}
